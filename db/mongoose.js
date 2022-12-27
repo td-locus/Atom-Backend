94fba2c22548ecf80eaae91bd38c316ca28e9ffd
Mohammad Muazam <mohammad@squadcast.com>
2022-12-27 16:39:52 +0530

    chore: added staging logic to mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,7 +3,22 @@ import dotenv from "dotenv";
 import { logErrorToSentry } from "../utils/sentry/index.js";
 dotenv.config();
 
-const connectionURL = process.env.NODE_ENV !== "development" ? process.env.DB_URL_PROD : process.env.DB_URL_DEV;
+// const connectionURL = process.env.NODE_ENV !== "development" ? process.env.DB_URL_PROD : process.env.DB_URL_DEV;
+const ENV = process.env.NODE_ENV;
+let connectionURL = "";
+switch (ENV) {
+  case "development":
+    connectionURL = process.env.DB_URL_DEV;
+    break;
+  case "production":
+    connectionURL = process.env.DB_URL_PROD;
+    break;
+  case "staging":
+    connectionURL = process.env.DB_URL_STAGING;
+    break;
+  default:
+    connectionURL = process.env.DB_URL_DEV;
+}
 const connectMongoose = async () => {
   mongoose
     .connect(connectionURL, {
